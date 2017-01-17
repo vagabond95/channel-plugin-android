47e8cb995988e8a9125351fbaadd347578c75e14
Mika Jihoon Yoon <mika@zoyi.co>
2017-01-17 18:12:47 +0900

    Init

---
+++ app/src/main/java/com/zoyi/channel/plugin/android/sample/App.java
@@ -0,0 +1,13 @@
+package com.zoyi.channel.plugin.android.sample;
+
+import android.app.Application;
+
+/**
+ * Created by mika on 2017. 1. 17..
+ */
+public class App extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
