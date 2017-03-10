f95611028c5bfd8c061669abaf130b88adde9626
Ryan <ryan@zoyi.co>
2017-03-10 12:23:49 +0900

    Add OnChannelPluginChangedListener example

--- app/src/main/java/com/zoyi/channel/plugin/android/sample/UserActivity.java
+++ app/src/main/java/com/zoyi/channel/plugin/android/sample/UserActivity.java
@@ -3,23 +3,36 @@ package com.zoyi.channel.plugin.android.sample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 import com.zoyi.channel.plugin.android.ChannelPlugin;
+import com.zoyi.channel.plugin.android.OnChannelPluginChangedListener;
+import com.zoyi.channel.plugin.android.push.ChannelPushClient;
 
 /**
  * Created by mika on 2017. 2. 16..
  */
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements OnChannelPluginChangedListener {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_user);
+
+    ChannelPlugin.addOnChannelPluginChangedListener(this);
+
+    ChannelPushClient.handlePushMessage(this);
   }
 
   @Override
   protected void onDestroy() {
+    ChannelPlugin.removeOnChannelPluginChangedListener(this);
     ChannelPlugin.checkOut();
     Toast.makeText(this, "Check out (user)", Toast.LENGTH_SHORT).show();
     super.onDestroy();
+  }
+
+  @Override
+  public void badgeChanged(int count) {
+    Log.i("Badge Changed", count + "");
   }
 }
