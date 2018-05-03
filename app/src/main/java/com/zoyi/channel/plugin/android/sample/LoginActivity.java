a68f2fc882b775792ead2110f8433217aca37468
vagabond95 <progjh@gmail.com>
2018-05-03 17:13:25 +0900

    Update plugin version to 3.0.4

--- app/src/main/java/com/zoyi/channel/plugin/android/sample/LoginActivity.java
+++ app/src/main/java/com/zoyi/channel/plugin/android/sample/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.zoyi.channel.plugin.android.sample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,12 +22,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
   private final static int APP_ID = 100;
 
   private EditText editId, editName, editPhoneNumber;
+  private Context context;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
     findViewById(R.id.login).setOnClickListener(this);
+
+    context = this;
 
     editId = (EditText) findViewById(R.id.id);
     editName = (EditText) findViewById(R.id.name);
@@ -54,7 +59,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     ChannelIO.boot(pluginSettings, guest, new OnBootListener() {
       @Override
       public void onCompletion(ChannelPluginCompletionStatus status) {
+        Log.d("test", status.toString());
         switch (status) {
+          case SUCCESS:
+            ChannelIO.open(context);
+            break;
           default:
             Toast.makeText(LoginActivity.this, "Check in failed", Toast.LENGTH_SHORT).show();
             break;
